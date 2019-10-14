abd6ea532680da48ad8a5f822d9d3e9ef1cb724f
dariozubaray <dzubaray@gmail.com>
2019-10-14 13:54:54 -0300

    agregando las escuchas de eventos en la clase sockets.ts

--- classes/Server.ts
+++ classes/Server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { SERVER_PORT } from '../global/environment';
 import socketIO from 'socket.io';
 import http from 'http';
+import * as socket from '../sockets/sockets';
 
 export default class Server {
     private static _instance: Server;
@@ -26,7 +27,10 @@ export default class Server {
     private escucharSockets() {
         console.log('Escuchando conexiones - sockets');
         this.io.on('connection', cliente => {
-            console.log('Nuevo cliente conectado', cliente);
+            console.log('Nuevo cliente conectado');
+
+            socket.desconectar(cliente);
+            socket.mensaje(cliente);
         });
     }
 
